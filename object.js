84c54f8113042fe045e74793652da3a5009fa560
hemant <hemant99arora@gmail.com>
2022-07-06 11:47:39 +0530

    object in js

---
+++ object.js
@@ -0,0 +1,13 @@
+let college = {
+    pid : 1234,
+    Name: "Hemant " ,
+    department: "Cse",
+    course:"b.tech",
+
+};
+console.log(college);
+let games = {
+    Name:"cricket,vollyball,chess",
+    tournament:"indoor,outdoor",
+};
+console.log(games.tournament);
